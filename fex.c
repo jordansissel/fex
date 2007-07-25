ce0128fd98f63e8667a572d9475c005a90c5888d
psionic <psionic@66067f73-fe4c-0410-82e9-b9c6d0c95a22>
2007-07-25 07:10:31 +0000

    - strlist api added for making lists of strings, yay.
    git-svn-id: https://semicomplete.googlecode.com/svn/fex@1492 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- fex.c
+++ fex.c
@@ -1,23 +1,58 @@
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char *prog = NULL;
 
 #define READBUFSIZE (64<<10)
 
+typedef struct strlist {
+  char **items;
+  int nitems;
+  int max_items;
+} strlist_t;
+
 void process_line(char *buf, int len, int argc, char **argv);
 void extract(char *format, char *buf);
-void tokenize(char ***tokens, int *ntokens, char *buf, char *sep);
-void freelist(char ***list);
+void tokenize(strlist_t **tokens, char *buf, char *sep);
+
+strlist_t* strlist_new();
+void strlist_free(strlist_t *list);
+void strlist_append(strlist_t *list, char *str);
+
+strlist_t* strlist_new() {
+  strlist_t *list;
+  list = malloc(sizeof(strlist_t));
+
+  list->max_items = 10;
+  list->nitems = 0;
+  list->items = malloc(list->max_items * sizeof(char*));
+
+  return list;
+}
+
+void strlist_free(strlist_t *list) {
+  int i;
+  for (i = 0; i < list->nitems; i++)
+    free(list->items[i]);
+  free(list);
+}
+
+void strlist_append(strlist_t *list, char *str) {
+  list->items[list->nitems] = strdup(str);
+
+  list->nitems++;
+  if (list->nitems == list->max_items) {
+    list->max_items *= 2;
+    list->items = realloc(list->items, list->max_items * sizeof(char *));
+  }
+}
 
 int main(int argc, char **argv) {
   char buf[READBUFSIZE];
-  char *sep = strdup(" ");
-  char *line = strdup("Hello there how are you");
-  char **tokens = NULL;
-  int ntoks = 0;
 
   prog = *argv;
   argv++;
@@ -25,20 +60,24 @@ int main(int argc, char **argv) {
 
   memset(buf, 0, READBUFSIZE);
 
-  //tokenize(&tokens, &ntoks, line, sep);
-  //int i;
-  //for (i = 0; i < ntoks; i++) {
-    //printf("%d: %s\n", i, tokens[i]);
-  //}
-//
-  //freelist(&tokens);
+
+  if (0) { 
+    strlist_t *tokens;
+    tokenize(&tokens, "asdf asdf asdf one two three four", " ");
+    int i;
+    for (i = 0; i < tokens->nitems; i++) {
+      printf("%d: %s\n", i, tokens->items[i]);
+    }
+    strlist_free(tokens);
+    return 2;
+  }
 
   while (NULL != fgets(buf, READBUFSIZE, stdin)) {
     int len;
     len = strlen(buf);
     len--; /* skip EOL */
     buf[len] = '\0'; /* Turn EOL into null */
-    //printf("String: '%s'\n", buf);
+    printf("String: '%s'\n", buf);
     process_line(buf, len, argc, argv);
   }
 
@@ -48,6 +87,7 @@ int main(int argc, char **argv) {
 void process_line(char *buf, int len, int argc, char **argv) {
   int i;
   for (i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
     extract(argv[i], buf);
     printf(" ");
   }
@@ -55,99 +95,74 @@ void process_line(char *buf, int len, int argc, char **argv) {
 }
 
 void extract(char *format, char *buf) {
-  char *tmpbuf = NULL, *tmpstart;
-  char *strptr = NULL;
   char *sep = NULL;
-  int fieldnum;
-  char **tokens = NULL;
-  int ntokens = 0;
 
   /* Because string literals aren't writable */
   sep = strdup(" ");
 
-  //printf("extract: %s\n", format);
+  printf("extract: %s\n", format);
 
   while (format[0] != '\0') {
-    char *tok;
+    strlist_t *tokens;
+    strlist_t *fields;
+    char *fieldstr;
 
-    /* if format[0] == '{', loop tokenizing on commas until '}' */
-    /*
-     * from '{'
-     *   append field(s) to new string
-     *   Fields are a number, or number:number, or -number
-     *   :number and number: must be valid
-     *   number:number is an inclusive range
-     *
-     */
+    tokenize(&tokens, buf, sep);
 
-    tokenize(&tokens, &ntokens, buf, sep);
-
-    if (format[0] == '{') {
-      char **fields = NULL;
-      int nfields;
+    /* All of these cases will advance the format string position */
+    /* This if case is a reallly lame hack */
+    if (isdigit(format[0]) || format[0] == '-') {
+      asprintf(&fieldstr, "%ld", strtol(format, &format, 10));
+    } else if (format[0] == '{') {
+      format++; /* Skip '{' */
       int fieldlen;
-      char *fieldstr;
       fieldlen = strcspn(format, "}") + 1;
       fieldstr = malloc(fieldlen * sizeof(char));
       memset(fieldstr, 0, fieldlen * sizeof(char));
       strncpy(fieldstr, format, fieldlen - 1);
-      printf("Block: %s\n", fieldstr);
-      free(fieldstr);
+    } else {
     }
 
-    freelist(&tokens);
+    tokenize(&fields, fieldstr, ",");
 
-    tmpbuf = tok;
-    if (format[0] != '\0') {
-      //printf("Shifting ahead by %d\n", tok - tmpbuf);
-      sep[0] = format[0];
-      format++;
+    for (int i = 0; i < fields->nitems; i++) {
+      int start, end;
+
     }
+    free(fieldstr);
+
+    //tmpbuf = tok;
+    //if (format[0] != '\0') {
+      ////printf("Shifting ahead by %d\n", tok - tmpbuf);
+      //sep[0] = format[0];
+      //format++;
+    //}
+    return;
+    strlist_free(fields);
+    strlist_free(tokens);
   }
 
-  printf("%s", tmpbuf);
+  //printf("%s", tmpbuf);
   //fprintf(stderr, "Distance: %d vs %d\n", tmpbuf - tmpstart, strlen(buf));
 
   free(sep);
-  free(tmpstart);
+  //free(tmpstart);
 }
 
-void tokenize(char ***tokens, int *ntokens, char *buf, char *sep) {
-  int token_size = 10;
+void tokenize(strlist_t **tokens, char *buf, char *sep) {
   char *strptr = NULL;
   char *tokctx;
   char *dupbuf = NULL;
   char *tok;
 
-  *ntokens = 0;
   dupbuf = strdup(buf);
   strptr = dupbuf;
 
-  *tokens = malloc(token_size * sizeof(char *));
-  memset(*tokens, 0, token_size * sizeof(char *));
+  *tokens = strlist_new();
 
   while ((tok = strtok_r(strptr, sep, &tokctx)) != NULL) {
     strptr = NULL;
-    (*tokens)[*ntokens] = strdup(tok);
-    (*ntokens)++;
-
-    /* Keep one spare token entry for a NULL entry */
-    if (*ntokens == token_size - 1) {
-      token_size *= 2;
-      *tokens = realloc(tokens, token_size * sizeof(char *));
-    }
+    strlist_append(*tokens, tok);
   }
-
-  /* Terminate the list with NULL */
-  (*tokens)[*ntokens] = NULL;
   free(dupbuf);
-}
-
-void freelist(char ***list) {
-  char **listp = *list;
-  while (**list != NULL) {
-    free(**list);
-    (*list)++;
-  }
-  free(*listp);
 }
